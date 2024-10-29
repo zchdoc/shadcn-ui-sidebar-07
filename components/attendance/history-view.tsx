db16e8b12cbff30adf9cb2977a6f1d0db94350f3
zchcpyw10 <571928489@qq.com>
2024-10-29 14:13:24 +0800

    处理查询日期bug

--- components/attendance/history-view.tsx
+++ components/attendance/history-view.tsx
@@ -41,12 +41,13 @@ export function HistoryView() {
   const [loading, setLoading] = React.useState(false)
   const {toast} = useToast()
   const isMobile = useMediaQuery("(max-width: 768px)")
+  // [attendanceRecords] 改为空数组，只在组件挂载时执行一次
   useEffect(() => {
     // 设置 startDate 默认为当天 00:00:00
     setStartDate(dayjs().startOf("day").toDate());
     // 设置 endDate 默认为当天 23:59:59
     setEndDate(dayjs().endOf("day").toDate());
-  }, [attendanceRecords]);
+  }, []);
   const fetchRecords = async () => {
     if (!employeeId || !startDate || !endDate) {
       toast({
