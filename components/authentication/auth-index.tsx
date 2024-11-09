e5f07e186a6977a27d03a69134e0b72a18f98ce9
ZchMac2020M1 <571928489@qq.com>
2024-11-09 16:36:29 +0800

    test new style

--- components/authentication/auth-index.tsx
+++ components/authentication/auth-index.tsx
@@ -17,14 +17,29 @@ export default function AuthenticationPage() {
     <div className="container relative min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 overflow-hidden">
       {/* Top bar */}
       <div className="absolute right-4 top-4 flex items-center gap-2 md:right-8 md:top-8 z-50">
-        <ThemeToggle/>
-        <Button
-          variant="outline"
-          onClick={() => setShowLogin(!showLogin)}
-          className="relative px-4 py-2 w-24"
-        >
-          {showLogin ? "Register" : "Login"}
-        </Button>
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-6 w-6"
+          >
+            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"/>
+          </svg>
+          Attendance System
+          <ThemeToggle/>
+          <Button
+            variant="outline"
+            onClick={() => setShowLogin(!showLogin)}
+            className="relative px-4 py-2 w-24"
+          >
+            {showLogin ? "Register" : "Login"}
+          </Button>
+        </div>
       </div>
       {/* Left side */}
       <div className={cn(
