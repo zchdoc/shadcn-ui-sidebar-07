78d4918310a2c8de3fe1b998884f562a7428f503
ZchMac2020M1 <571928489@qq.com>
2024-11-16 16:31:21 +0800

    Update auth-index.tsx

--- components/authentication/auth-index.tsx
+++ components/authentication/auth-index.tsx
@@ -87,6 +87,7 @@ export default function AuthenticationPage() {
 
       setContentHeight(maxHeight);
       setContentWidth(maxWidth);
+      setAnimationType('fade');
     };
 
     updateDimensions();
